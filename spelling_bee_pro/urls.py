916369a5c7d38ae93f8172ffe76f63e790169d4c
AsuraProgram <120566232+AsuraProgram@users.noreply.github.com>
2026-05-30 17:38:25 +0800

    Update urls.py

--- spelling_bee_pro/urls.py
+++ spelling_bee_pro/urls.py
@@ -7,12 +7,24 @@ from bee.views import game_view, save_score, leaderboard_view, my_scores_view, r
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', game_view, name='play_game'),
+
+    path('game-arena/', game_arena_view, name='game_arena'),
+
     path('leaderboard/', leaderboard_view, name='leaderboard'),
     path('my-scores/', my_scores_view, name='my_scores'),
+
     path('api/save-score/', save_score, name='save_score'),
-    path('login/', auth_views.LoginView.as_view(template_name='bee/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
+    path('login/', auth_views.LoginView.as_view(
+        template_name='bee/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='/login/'
+    ), name='logout'),
+
     path('register/', register_view, name='register'),
 ]
 
