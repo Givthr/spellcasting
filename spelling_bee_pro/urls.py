6a873d05acc7007f9ae63930f94f0f1a441671d6
AsuraProgram <120566232+AsuraProgram@users.noreply.github.com>
2026-06-01 23:14:36 +0800

    Update urls.py

--- spelling_bee_pro/urls.py
+++ spelling_bee_pro/urls.py
@@ -9,6 +9,15 @@ from bee.views import game_view, game_arena_view, save_score, leaderboard_view, 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sw.js', TemplateView.as_view(
+        template_name="sw.js", 
+        content_type='application/javascript'
+    ), name='sw.js'),
+    
+    path('manifest.json', TemplateView.as_view(
+        template_name="manifest.json", 
+        content_type='application/json'
+    ), name='manifest.json'),
 
     path('', game_view, name='play_game'),
 
