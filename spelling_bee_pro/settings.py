3797ee7da218c7ce723ad0d7d26506be7b5748aa
AsuraProgram <120566232+AsuraProgram@users.noreply.github.com>
2026-06-01 22:33:45 +0800

    Update settings.py

--- spelling_bee_pro/settings.py
+++ spelling_bee_pro/settings.py
@@ -103,6 +103,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login
 
 # --- CSRF and Security Settings for Production Environments ---
 if not DEBUG:
