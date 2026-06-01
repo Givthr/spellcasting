79e5a4bd77936010ba6ec7c1fb2663ab231cf2c5
AsuraProgram <120566232+AsuraProgram@users.noreply.github.com>
2026-06-01 23:44:43 +0800

    Create sw.js

---
+++ bee/templates/bee/sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'spellcasting-v1';
+const ASSETS_TO_CACHE = [
+  '/login/',
+  '/logout/'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
